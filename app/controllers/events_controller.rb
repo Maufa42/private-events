ebcaa561c18396650cb2db90d2dee6c647aad8bb
Aryan Pal <aryanpalongraph@gmail.com>
2022-07-27 18:47:07 +0530

    💄 Added Profile_gravatar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,31 @@
 class EventsController < ApplicationController
 
 def index
+  @posts = Post.all
+  @post = Post.new
+end
+
+def new
+  @post = Post.new
+end
+
+def create
+  @post = Post.new(post_params)
+
+  if @post.save
+    redirect_to @post
+  else
+    render :new
+  end
+end
+
+def about
+end
+
+private
+
+def post_params
+ params.require(@post).permit(:title,:content,:user_id)
 end
 
 end
